badf9659fc2bc330309b6305955796d0dbbda996
kishanreddyd <88155670+kishanreddyd@users.noreply.github.com>
2021-09-08 19:32:23 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,13 +1,13 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://3.108.59.54:9000/',
+  serverUrl: 'http://3.109.213.148:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
 	    'sonar.login': 'admin',
-	    'sonar.password': 'admin',
+	    'sonar.password': 'admin123',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
